a4b08f96b5cc49e93d09c00b34162ec7bd302410
Lona Frießner <lona.friessner@gmail.com>
2022-02-04 15:13:09 +0100

    added styling and reload after win/lose

--- java.js
+++ java.js
@@ -8,26 +8,18 @@ function computerPlay() {
 	(randomNumber === 2) ? "paper" :
 	"scissors";
 }
-//überprüfe, ob Player gewonnen hat:
-	//P Scissors & C Paper
-	//P Paper & C Rock
-	//P Rock & C Scissors
-	//Zeige eine Nachricht "Du hast gewonnen: playerSelection beats computerSelection"
-//überprüfe, ob beide das Gleiche haben
-	//Zeige: "Keiner hat gewonnen; beide zeigten playerSelection"
-//wenn computer gewinnt (Fälle andersrum als oben)
-	//zeige Nachricht: "You lose. computerSelection beats playerSelection"
-//bei falscher oder keiner Angabe (else)
-	//zeige: please insert paper, rock or stone
 
 let rockbtn = document.getElementById("rock");
 let paperbtn = document.getElementById("paper");
 let scissorsbtn = document.getElementById("scissors");
 let resultDiv = document.querySelector('.results');
-let points = document.querySelector('.points');
+let compPointsDisplay = document.querySelector('.points-comp');
+let playerPointsDisplay = document.querySelector('.points-player');
 let playerPoints = 0;
 let computerPoints = 0;
-points.textContent = "Player: " + playerPoints + "      " + "computer:   "+ computerPoints;
+compPointsDisplay.textContent = computerPoints;
+playerPointsDisplay.textContent = playerPoints;
+
 const buttons = document.querySelectorAll("button");
 
 
@@ -46,10 +38,14 @@ function playRound(computerSelection, playerSelection) {
 			resultDiv.textContent = `ONE POINT FOR YOU! ${playerSelection} BEATS ${computerSelection}`;
 			++playerPoints;
 			if (playerPoints >= 5) {
-				points.textContent = "VICTORYYYYYY! YOU WON THE GAME!"
+				resultDiv.textContent = "VICTORYYYYYY! YOU WON THE GAME! " + String.fromCodePoint(128079);
+				compPointsDisplay.textContent = String.fromCodePoint(128555);
+				playerPointsDisplay.textContent = String.fromCodePoint(129395);
+				setTimeout(location.reload(), 3000);
 			}
 			else {
-				points.textContent = "Player: " + playerPoints + "      " + "computer:   "+ computerPoints;
+				compPointsDisplay.textContent = computerPoints;
+				playerPointsDisplay.textContent = playerPoints;
 			}
 		}
 		else if (
@@ -59,10 +55,14 @@ function playRound(computerSelection, playerSelection) {
 			resultDiv.textContent = `OUCH! COMPUTER BEAT YOU WITH ${computerSelection}`;
 			++computerPoints;
 			if (computerPoints >= 5) {
-				points.textContent = "OH NOOOO! YOU LOST THE GAME"
+				resultDiv.textContent = "OH NOOOO! YOU LOST THE GAME " + String.fromCodePoint(0x2620);				;
+				compPointsDisplay.textContent = String.fromCodePoint(129395);
+				playerPointsDisplay.textContent = String.fromCodePoint(128555);
+				window.setTimeout(location.reload(), 5000);
 			}
 			else {
-				points.textContent = "Player: " + playerPoints + "      " + "computer:   "+ computerPoints;
+				compPointsDisplay.textContent = computerPoints;
+				playerPointsDisplay.textContent = playerPoints;
 			}
 		}
 
@@ -74,39 +74,3 @@ function playRound(computerSelection, playerSelection) {
 			resultDiv.textContent = `Something went wrong, try again`;
 		}
 }
-
-
-//5 Runden; in jeder Runde soll Spieler gefragt werden, was er wählt
-//in jeder RUnde soll der Computer neu entscheiden
-//nach jeder Runde soll die Punktzahl registriert werden
-//in jeder Runde soll der Punktstand registriert werden
-//am Ende soll der Endstand ausgegeben werden
-//
-// function game() {
-// 	let playerPoints = 0;
-// 	let computerPoints = 0;
-// 	for (let i = 0; i < 5; i++) {
-// 		let playerSelection = prompt ("What do you choose?");
-// 		let computerSelection = computerPlay();
-// 		console.log(playerSelection);
-// 		console.log(computerSelection);
-// 		if (playRound(computerSelection,playerSelection) === 1) {
-// 			++playerPoints;
-// 			console.log(`Yeah: Your ${playerSelection.toUpperCase()} beats the computer's ${computerSelection.toUpperCase()}.`)
-// 		}
-// 		else if (playRound(computerSelection,playerSelection) === 2) {
-// 			++computerPoints;
-// 			console.log(`Oh dear: Your ${playerSelection.toUpperCase()} has been beaten by the computer's ${computerSelection.toUpperCase()}.`)
-// 		}
-// 		else if (playRound(computerSelection,playerSelection) === 3) {
-// 			console.log(`Boo, are you soulmates? Both chose ${computerSelection.toUpperCase()}`)
-// 		}
-// 		else {
-// 			console.log("Man, please type anything valid the next time.")
-// 		}
-// 	console.log("Spielstand: " + playerPoints + " (du) zu " + computerPoints + " (Computer).");
-// 	}
-// 	console.log("Endstand: " + playerPoints + " zu " + computerPoints + " (Computer).");
-// }
-//
-// game();
